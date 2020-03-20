ca1efbc5956ce518d323a204da496fe54ca09235
marecheck <marecheck98@gmail.com>
2020-03-20 20:54:49 +0100

    Add line main.cpp

--- main.cpp
+++ main.cpp
@@ -4,5 +4,6 @@ int main() {
     std::cout << "Hello, World!" << std::endl;
     std::cout<<"No fajnie jest"<<std::endl;
     cout<<"Powinno śmigać"<<endl;
+    cout<<"A walne se commita a co mi tam"
     return 0;
 }
