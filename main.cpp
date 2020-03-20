7cebd5152998f42daacca1e2a092e2b55404cb90
marecheck <marecheck98@gmail.com>
2020-03-20 20:54:20 +0100

    Add line main.cpp

--- main.cpp
+++ main.cpp
@@ -1,7 +1,8 @@
 #include <iostream>
-
+using namespace std;
 int main() {
     std::cout << "Hello, World!" << std::endl;
     std::cout<<"No fajnie jest"<<std::endl;
+    cout<<"Powinno śmigać"<<endl;
     return 0;
 }
