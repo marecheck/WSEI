f9f5ea1da2528f5c338349ddd7a5c1823ebf362a
marecheck <marecheck98@gmail.com>
2020-03-21 19:50:12 +0100

    cokolwiek

--- main.cpp
+++ main.cpp
@@ -1,11 +1,13 @@
 #include <iostream>
 using namespace std;
 int main() {
+    string cokolwiek;
     std::cout << "Hello, World!" << std::endl;
     std::cout<<"No fajnie jest"<<std::endl;
     cout<<"Powinno śmigać"<<endl;
     cout<<"A walne se commita a co mi tam"<<endl;
     cout<<"O a teraz Slack no no"<<endl;
     cout<<"napisz cokolwiek"<<endl;
+    cin>>cokolwiek;
     return 0;
 }
